e2f0cc4dc1c2437a24b7c2a670169a87d83f0e69
sententia7 <winterer02@gmail.com>
2022-05-28 21:40:58 +0900

    Initial commit

---
+++ JavaAlgorithm/src/main/java/javaalgorithm/ch00/ServletInitializer.java
@@ -0,0 +1,13 @@
+package javaalgorithm.ch00;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+public class ServletInitializer extends SpringBootServletInitializer {
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(Ch00Application.class);
+    }
+
+}
---
+++ JavaAlgorithm/src/test/java/javaalgorithm/ch00/Ch00ApplicationTests.java
@@ -0,0 +1,13 @@
+package javaalgorithm.ch00;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Ch00ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
